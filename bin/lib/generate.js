c26535b874cdbd764d02721f2aab7a84fd8f1f80
shaodahong <darryshaw@gmail.com>
2018-08-02 23:09:04 +0800

    add generate

--- bin/lib/generate.js
+++ bin/lib/generate.js
@@ -1,3 +1,22 @@
-module.exports = async params => {
+const Metalsmith = require("metalsmith")
+const Handlebars = require("handlebars")
+const path = require("path")
 
-}
+const metalsmith = Metalsmith(path.join(process.cwd(), "/lib/mobile"))
+
+metalsmith
+  .metadata({
+    destDirName: 'demo',
+    inPlace: process.cwd(),
+    noEscape: true
+  })
+  .clean(false)
+  .source(".")
+  .destination(process.cwd())
+  .build((err, files) => {
+    console.log(err, files)
+  })
+
+// module.exports = async params => {
+
+// }
