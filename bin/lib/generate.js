94156d84434ed2ae60be4658e59b55e1cefc1926
shaodahong <337097324@qq.com>
2018-08-13 17:42:56 +0800

    添加命令行进度

--- bin/lib/generate.js
+++ bin/lib/generate.js
@@ -3,32 +3,36 @@ const Handlebars = require('handlebars')
 const path = require('path')
 module.exports = async (params, targetDir) => {
   const metalsmith = Metalsmith(path.join(process.cwd(), '/lib/mobile'))
-  console.log(1, params, targetDir)
-  metalsmith
-    .metadata({
-      destDirName: '/',
-      inPlace: targetDir,
-      noEscape: true
-    })
-    .clean(false)
-    .source('.')
-    .destination(targetDir)
-    .use((files, metalsmith, done) => {
-      Object.keys(files).forEach(fileName => {
-        try {
-          if (/\.js$|\.jsx$|\.json$|\.md$/.test(fileName)) {
-            console.log('fileName', fileName)
-
-            const t = files[fileName].contents.toString()
-            files[fileName].contents = new Buffer(Handlebars.compile(t)(params))
-          }
-        } catch (error) {
-          console.log(`\n Template compile Error: ${error}`)
-        }
+  return new Promise((resole, reject) => {
+    metalsmith
+      .metadata({
+        destDirName: '/',
+        inPlace: targetDir,
+        noEscape: true
       })
-      done()
-    })
-    .build((err, files) => {
-      console.log(err, files)
-    })
+      .clean(false)
+      .source('.')
+      .destination(targetDir)
+      .use((files, metalsmith, done) => {
+        Object.keys(files).forEach(fileName => {
+          try {
+            if (/\.js$|\.jsx$|\.json$|\.md$/.test(fileName)) {
+              const t = files[fileName].contents.toString()
+              files[fileName].contents = new Buffer(
+                Handlebars.compile(t)(params)
+              )
+            }
+          } catch (error) {
+            // console.log(`\n Template compile Error: ${error}`)
+          }
+        })
+        done()
+      })
+      .build((err, files) => {
+        if (err) {
+          // reject(`\n Template build Error: ${err}`)
+        }
+        resole()
+      })
+  })
 }
--- bin/mobile.js
+++ bin/mobile.js
@@ -2,11 +2,14 @@ const path = require('path')
 const fs = require('fs-extra')
 const q = require('inquirer')
 const chalk = require('chalk')
+const shell = require('shelljs')
+const ora = require('ora')
+
 const generate = require('./lib/generate')
+
 module.exports = async (params = {}) => {
   const inCurrent = !params.new
   const targetDir = path.resolve(params.dir || '.')
-  console.log('fs.existsSync(targetDir)', fs.existsSync(targetDir))
 
   if (fs.existsSync(targetDir) && !inCurrent) {
     const { result } = await q.prompt([
@@ -25,11 +28,30 @@ module.exports = async (params = {}) => {
     if (!result) {
       return
     } else if (result === 'overwrite') {
-      console.log(`\n正在删除 ${chalk.red(targetDir)}...`)
+      console.log(`\n正在删除 ${chalk.red(targetDir)}...\n`)
       await fs.remove(targetDir)
     }
   }
   await fs.ensureDir(targetDir)
 
+  const spinner = ora('开始生成模板').start()
+
   await generate(params, targetDir)
+
+  spinner.color = 'yellow'
+  spinner.text = '安装项目依赖'
+
+  if (!inCurrent) {
+    shell.cd(targetDir)
+  }
+
+  const child = shell.exec('yarn', {
+    silent: true,
+    async: true
+  })
+
+  child.stdout.on('close', function(data) {
+    spinner.succeed('完成安装')
+  })
+
 }
