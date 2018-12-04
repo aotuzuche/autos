2469efb02326855820bfb4b2bfff14715b64a30b
shaodahong <337097324@qq.com>
2018-12-04 14:20:36 +0800

    fix: fix @babel/plugin-proposal-decorators 未设置 decoratorsBeforeExport 导致编译报错

--- bin/build/config/babel.config.js
+++ bin/build/config/babel.config.js
@@ -38,14 +38,14 @@ module.exports = {
     ],
     '@babel/plugin-syntax-dynamic-import',
     '@babel/plugin-syntax-import-meta',
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-json-strings',
     [
       '@babel/plugin-proposal-decorators',
       {
-        legacy: true
+        decoratorsBeforeExport: true
       }
     ],
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-json-strings',
     '@babel/plugin-proposal-function-sent',
     '@babel/plugin-proposal-export-namespace-from',
     '@babel/plugin-proposal-numeric-separator',
