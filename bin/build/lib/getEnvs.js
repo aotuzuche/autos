d48ef1369a3c55ec3a7e5849b402037b61d58cbf
shao <darryshaw@gmail.com>
2019-04-19 18:32:14 +0800

    perf: 优化了 webpack 环境变量的设置

---
+++ bin/build/lib/getEnvs.js
@@ -0,0 +1,21 @@
+module.exports = () => {
+  const raw = Object.keys(process.env)
+    .filter(key => /^AT_/i.test(key))
+    .reduce(
+      (env, key) => {
+        env[key] = process.env[key]
+        return env
+      },
+      {
+        NODE_ENV: process.env.NODE_ENV || 'development',
+        PACKAGE: process.env.PACKAGE || 'development'
+      }
+    )
+  // Stringify all values so we can feed into Webpack DefinePlugin
+  const stringified = Object.keys(raw).reduce((env, key) => {
+    env[`process.env.${key}`] = JSON.stringify(raw[key])
+    return env
+  }, {})
+
+  return stringified
+}
