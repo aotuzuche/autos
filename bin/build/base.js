f371a07c94a50c521403c81faf7d1d3956234b63
shao <darryshaw@gmail.com>
2019-05-07 16:53:23 +0800

    fix: 🐛 base.js
    修复 resolveEntry 错别字

--- bin/build/base.js
+++ bin/build/base.js
@@ -43,7 +43,7 @@ function resolveEntry() {
   const supportEntrySuffixs = ['js', 'jsx', 'ts', 'tsx']
 
   for (const suffix of supportEntrySuffixs) {
-    const path = resolveProjectPath(`src/mian.${suffix}`)
+    const path = resolveProjectPath(`src/main.${suffix}`)
     if (fs.existsSync(path)) {
       return path
     }
