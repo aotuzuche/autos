328f4b092a49d328fd8c3c8abb32c5da4ef72b5f
shaodahong <darryshaw@gmail.com>
2020-06-12 15:12:09 +0800

    fix: checkGitClean if not a git repository

--- bin/lib/checkGitClean.js
+++ bin/lib/checkGitClean.js
@@ -2,9 +2,17 @@ const execa = require('execa')
 const logWithExit = require('./logWithExit')
 
 module.exports = function checkGitClean() {
-  const result = execa.sync('git', ['status', '--porcelain'], {
-    preferLocal: false,
-  })
+  let result
+
+  try {
+    result = execa.sync('git', ['status', '--porcelain'], {
+      preferLocal: false,
+    })
+  } catch (error) {
+    if (!/not a git repository/.test(error.message)) {
+      return logWithExit(error)
+    }
+  }
 
   if (result.stdout && result.stdout.trim()) {
     logWithExit('本地有改动的文件未提交！！！')
