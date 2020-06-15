f0a5fec86649d893251c64256259eb4f9abeaa1f
shaodahong <darryshaw@gmail.com>
2020-06-15 15:34:06 +0800

    ci: verify commit msg

--- scripts/verify-commit-msg.js
+++ scripts/verify-commit-msg.js
@@ -5,7 +5,7 @@ const msg = require('fs').readFileSync(msgPath, 'utf-8').trim()
 
 const commitRE = /^(revert: )?(feat|fix|polish|docs|style|refactor|perf|test|workflow|ci|chore|types|build|v)(\(.+\))?: .{1,50}/
 
-if (!commitRE.test(msg)) {
+if (!/^v\d+\.\d+\.\d+/.test(msg) && !commitRE.test(msg)) {
   console.log()
   console.error(`  ${chalk.bgRed.white(' ERROR ')} 无效的提交信息`)
   process.exit(1)
