9863d139152489d9ab13ac0920419a50be41d79a
shaodahong <darryshaw@gmail.com>
2020-06-18 17:33:38 +0800

    refactor: remove unuse webpack plugin

--- bin/build/auto-prod-filter-loader/lib/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = source => {
-  if (process.env.PACKAGE === 'production') {
-    const match = /\/\/\s*?>>>(\s|.)*?\/\/\s*?<<</g
-    return source.replace(match, '')
-  }
-  return source
-}
